aea27cb575116a907341fde4afd4afcc8f7ee35a
Robert J Bare III <rjbare3@gmail.com>
2023-05-12 07:23:39 -0400

    Added basic startScript creation

--- modules/setup.py
+++ modules/setup.py
@@ -2,11 +2,25 @@ import os
 import modules.ui as ui
 from decouple import config
 
-cwd = os.getcwd()
-serversDir = os.path.join(cwd, config('serversDirectory'))
+serversDir = os.path.join(os.getcwd(), config('serversDirectory'))
 
 def init():
     if not os.path.exists(serversDir):
         print("servers directory does not exist, creating...")
         os.mkdir(serversDir)
-        print("server directory created, visit: "+ui.link("https://www.minecraft.net/en-us/download/server")+" to download the most recent server.jar to the sampleServer directory to get started")
+        print(str(serversDir)+" created, creating sampleServer directory...")
+        os.mkdir(os.path.join(serversDir,'sampleServer'))
+        basicStartScript('sampleServer')
+        print("sampleServer directory and start script created, Visit: "+ui.link("https://www.minecraft.net/en-us/download/server")+" to download the most recent server.jar to the sampleServer directory to get started")
+
+def make_executable(path):
+    mode = os.stat(path).st_mode
+    mode |= (mode & 0o444) >> 2    # copy R bits to X
+    os.chmod(path, mode)
+
+def basicStartScript(serverStartDir):
+    scriptLoc = os.path.join(serversDir,serverStartDir,"startServer.sh")
+    script = open(scriptLoc, 'w')
+    script.write("java -Xmx1G -Xms1G -jar server.jar")
+    script.close()
+    make_executable(scriptLoc)
