b6f28822ae7d9f72f61e8d62d312a0a99e01f1b7
BuildTools <unconfigured@null.spigotmc.org>
2023-05-12 05:20:13 -0400

    Cleaned up core.py

--- modules/core.py
+++ modules/core.py
@@ -1,10 +1,14 @@
+#Imports
 import os
 from decouple import config
 from decouple import Csv
 from pathlib import Path
-cwd = os.getcwd()
 
+#Needed Variables
+cwd = os.getcwd()
 starters = (config('starters',cast=Csv()))
+
+#Functions
 def findStarter(serverDir):
     for value in starters:
         if (Path(os.path.join(cwd,'servers',serverDir,value))).is_file():
