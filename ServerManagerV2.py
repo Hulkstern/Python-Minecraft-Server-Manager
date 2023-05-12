6b1565998168ae1bc68367dc0fb19820f586208b
BuildTools <unconfigured@null.spigotmc.org>
2023-05-12 05:18:50 -0400

    Built out findStarter functions

--- ServerManagerV2.py
+++ ServerManagerV2.py
@@ -4,12 +4,11 @@ import modules.server as svr
 import modules.servers as svrs
 import modules.ui as ui
 import modules.setup as setup
+import modules.core as core
 from decouple import config
 
 cwd = os.getcwd()
 serversDir = os.path.join(cwd,'servers')
-
-#print(os.getcwd()) #this be where we be
 
 #Initial Setup
 setup.init()
@@ -20,3 +19,5 @@ folder = os.path.join(cwd,'servers')
 sub_folders = [name for name in os.listdir(serversDir) if os.path.isdir(os.path.join(serversDir, name))]
 
 print(sub_folders)
+
+print(core.findStarter(sub_folders[1]))
--- modules/core.py
+++ modules/core.py
@@ -1,8 +1,12 @@
 import os
 from decouple import config
 from decouple import Csv
+from pathlib import Path
+cwd = os.getcwd()
 
 starters = (config('starters',cast=Csv()))
-def findStarter():
-
-    return
+def findStarter(serverDir):
+    for value in starters:
+        if (Path(os.path.join(cwd,'servers',serverDir,value))).is_file():
+            return value
+    return False
